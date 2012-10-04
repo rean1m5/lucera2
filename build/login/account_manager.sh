eff31e9e7473fd1bd5092e158d6ce27797daaad4
Ro0TT <topp3000@mail.ru>
2012-10-04 16:15:10 +0400

    Билд

--- build/login/account_manager.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -Xbootclasspath/p:libs/crypt.jar -Djava.util.logging.config.file=console.cfg -cp ../libs/*:./login.jar ru.catssoftware.accountmanager.AccountManager
--- build/login/register_gameserver.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-java -cp ../libs/*;./login.jar  ru.catssoftware.gsregistering.GameServerRegister
