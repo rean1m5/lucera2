5cc4837f1e49e6de3a5efa57b6ef5905ad97f46a
Ro0TT <topp3000@mail.ru>
2012-10-26 23:20:55 +0400

    Исправлен скилл Spell Turning у Hierophant, теперь сбивает каст, а не обнуляет цель у игрока.

---
+++ gameserver/src/ru/catssoftware/gameserver/skills/effects/EffectInterrupt.java
@@ -0,0 +1,66 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ru.catssoftware.gameserver.skills.effects;
+
+import ru.catssoftware.gameserver.ai.CtrlIntention;
+import ru.catssoftware.gameserver.model.L2Effect;
+import ru.catssoftware.gameserver.skills.Env;
+import ru.catssoftware.gameserver.templates.skills.L2EffectType;
+
+/*
+ * @author Ro0TT
+ * @date 26.10.2012
+ */
+
+/**
+ * @author -Nemesiss-
+ *
+ */
+public final class EffectInterrupt extends L2Effect
+{
+	public EffectInterrupt(Env env, EffectTemplate template)
+	{
+		super(env, template);
+	}
+
+	@Override
+	public L2EffectType getEffectType()
+	{
+		return L2EffectType.INTERRUPT;
+	}
+
+	/** Notify started */
+	@Override
+	public boolean onStart()
+	{
+		getEffected().abortCast();
+		getEffected().getAI().setIntention(CtrlIntention.AI_INTENTION_IDLE, getEffector());
+		return true;
+	}
+
+	/** Notify exited */
+	@Override
+	public void onExit()
+	{
+		// nothing
+	}
+
+	@Override
+	public boolean onActionTime()
+	{
+		// nothing
+		return false;
+	}
+}
